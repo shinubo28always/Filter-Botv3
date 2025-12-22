02357dcbe0d04dc9a68b8a752047c06f332acaa6
shinubo28always <shinubo28always@gmail.com>
2025-12-22 11:45:35 +0600

    Refactor database connection and add filter management
    Updated MongoDB database name and modified user ID handling to ensure string conversion. Added functions to delete filters and retrieve all filters.

--- database.py
+++ database.py
@@ -1,9 +1,8 @@
 from pymongo import MongoClient
 import config
 
-# MongoDB Connection
 client = MongoClient(config.MONGO_URI)
-db = client['anime_filter_bot']
+db = client['anime_bot_db']
 
 users = db['users']
 filters = db['filters']
@@ -11,7 +10,7 @@ settings = db['settings']
 admins = db['admins']
 
 def add_user(uid):
-    users.update_one({"_id": uid}, {"$set": {"_id": uid}}, upsert=True)
+    users.update_one({"_id": str(uid)}, {"$set": {"_id": str(uid)}}, upsert=True)
 
 def is_admin(uid):
     if str(uid) == str(config.OWNER_ID): return True
@@ -23,11 +22,20 @@ def add_filter(keyword, data):
 def get_filter(keyword):
     return filters.find_one({"keyword": keyword.lower()})
 
+def delete_filter(keyword):
+    return filters.delete_one({"keyword": keyword.lower()}).deleted_count
+
+def delete_all_filters():
+    return filters.delete_many({}).deleted_count
+
 def get_all_keywords():
     return [f['keyword'] for f in filters.find({}, {"keyword": 1})]
 
+def get_all_filters_list():
+    return list(filters.find({}, {"keyword": 1, "title": 1}))
+
 def set_fsub(cid):
-    settings.update_one({"key": "fsub"}, {"$set": {"value": cid}}, upsert=True)
+    settings.update_one({"key": "fsub"}, {"$set": {"value": str(cid)}}, upsert=True)
 
 def get_fsub():
     res = settings.find_one({"key": "fsub"})
