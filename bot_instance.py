95939267e87e4ec59aa8ee256ca17dd9876f3268
shinubo28always <shinubo28always@gmail.com>
2025-12-23 21:09:05 +0600

    Update bot_instance.py

--- bot_instance.py
+++ bot_instance.py
@@ -1,3 +1,6 @@
 import telebot
 import config
 bot = telebot.TeleBot(config.API_TOKEN, parse_mode='HTML')
+# Please Support Us! @DogeshBhai_Pure_Bot on Telegram! 
+# This Bot Created By: @AniReal_Support!
+# Join & Support Us! @DogeshBhai_Pure_Bot
