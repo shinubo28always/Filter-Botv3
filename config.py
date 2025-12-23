cfab12f8d80da63fcf77c8877fe03e8a4f31417c
shinubo28always <shinubo28always@gmail.com>
2025-12-23 09:20:24 +0600

    Update config.py

--- config.py
+++ config.py
@@ -4,12 +4,13 @@ from dotenv import load_dotenv
 load_dotenv()
 
 def get_config(key, default=None):
+    # Priority: 1. Hardcoded, 2. .env, 3. Host Env
     hardcoded = {
         "API_TOKEN": "", 
         "OWNER_ID": "", 
         "MONGO_URI": "",
-        "DB_CHANNEL_ID": "-1003202118558",
-        "LOG_CHANNEL_ID": "-1003202118558"
+        "DB_CHANNEL_ID": "",
+        "LOG_CHANNEL_ID": ""
     }
     val = hardcoded.get(key) or os.getenv(key)
     return val if val else default
@@ -20,9 +21,9 @@ MONGO_URI = get_config("MONGO_URI")
 DB_CHANNEL_ID = int(get_config("DB_CHANNEL_ID")) if get_config("DB_CHANNEL_ID") else 0
 LOG_CHANNEL_ID = int(get_config("LOG_CHANNEL_ID")) if get_config("LOG_CHANNEL_ID") else 0
 
-# Static
+# Static Assets
 STICKER_ID = "CAACAgUAAxkBAAEP4flpKC6Ozwtd25givMwrN3zMcnLeFQACuBYAArKmaFa__rW3azdtFjYE"
 EFFECT_FIRE = "5104841245755180586"
 EFFECT_PARTY = "5046509860389126442"
 
-LINK_ANIME_CHANNEL = "https://t.me/Anime_Dekhbo_Official"
+LINK_ANIME_CHANNEL = "https://t.me/DogeshBhai_Pure_Bot"
