ae0f3a371b19af8ae6c93d50365c40010c6074af
shinubo28always <shinubo28always@gmail.com>
2026-01-11 17:30:11 +0600

    Update auth.py

--- plugins/auth.py
+++ plugins/auth.py
@@ -1,5 +1,5 @@
-# Please Support Us! @DogeshBhai_Pure_Bot on Telegram! 
-# This Bot Created By: @AniReal_Support!
+# Please Support Us! @UNRATED_CODER on Telegram! 
+# This Bot Created By: @UNRATED_CODER!
 import html
 from bot_instance import bot
 import database as db
@@ -51,22 +51,51 @@ def handle_membership_security(message):
                 try: bot.leave_chat(chat.id)
                 except: pass
 
- # Please Support Us! @DogeshBhai_Pure_Bot on Telegram! 
- # This Bot Created By: @AniReal_Support!
+ # Please Support Us! @UNRATED_CODER on Telegram! 
+ # This Bot Created By: @UNRATED_CODER!
 
     # --- GROUP TRACKING ---
-    elif chat.type in ["group", "supergroup"]:
-        if new.status in ["administrator", "member"]:
-            db.add_group(chat.id, chat.title)
-        elif new.status in ["left", "kicked"]:
-            db.del_group(chat.id)
+elif chat.type in ["group", "supergroup"]:
+    if new.status in ["administrator", "member"]:
+        db.add_group(chat.id, chat.title)
+    elif new.status in ["left", "kicked"]:
+        db.del_group(chat.id)
 
 @bot.message_handler(content_types=['new_chat_members'])
 def on_join_group(message):
     for user in message.new_chat_members:
         if user.id == bot.get_me().id:
+            # DB update
             db.add_group(message.chat.id, message.chat.title)
-            bot.send_message(message.chat.id, "👋 <b>Bot Active!</b>\nType anime name to search.")
 
+            # GROUP_AUTH_MSG from config
+            # Safe logic for clickable group name
+            chat_name = message.chat.title
+            chat_link = None
 
-# Join & Support Us! @DogeshBhai_Pure_Bot
+            try:
+                # Check if chat is public or bot can get invite link
+                if message.chat.username:
+                    # Public group → t.me link
+                    chat_link = f"https://t.me/{message.chat.username}"
+                else:
+                    # Try to get invite link if bot is admin
+                    chat_info = bot.get_chat(message.chat.id)
+                    if chat_info.invite_link:
+                        chat_link = chat_info.invite_link
+            except Exception:
+                chat_link = None  # fallback
+
+            # Prepare message
+            if chat_link:
+                # clickable name
+                group_text = f"<a href='{chat_link}'>{chat_name}</a>"
+            else:
+                # just text
+                group_text = f"<b>{chat_name}</b>"
+
+            # Send message from config
+            msg_text = config.GROUP_AUTH_MSG.format(group_name=group_text)
+            bot.send_message(message.chat.id, msg_text, parse_mode="HTML")
+
+# Join & Support Us! @UNRATED_CODER!
